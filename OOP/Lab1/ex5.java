19d74b39117406935951f892081584f969610f07
MrrDevils <Huotmanut0@gmail.com>
2023-09-22 20:49:10 +0700

    From pc

--- OOP/Lab1/ex5.java
+++ OOP/Lab1/ex5.java
@@ -1,10 +1,64 @@
 package Lab1;
 
 public class ex5 {
+    private static boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
+
     public static void main(String[] args) {
         String text = "I Love My Home Country";
         int textLength = text.length();
+        System.out.println("A:");
+        System.out.println("Original Text: " + text);
+        System.out.println("Text length is: " + textLength + "\n");
+        // =================================================
+        System.out.println("B:");
+        text = "I love my little country";
+        String wordToRemove = "I love ";
 
-        System.out.println("Text length is: " + textLength);
+        if (text.contains(wordToRemove)) {
+            String newText = text.substring(0, text.indexOf(wordToRemove)) +
+                    text.substring(text.indexOf(wordToRemove) + wordToRemove.length());
+
+            System.out.println("Original Text: " + text);
+            System.out.println("Text to remove: " + wordToRemove);
+            System.out.println("Result: " + newText);
+        } else {
+            System.out.println("'" + wordToRemove + "' not found in the text.");
+        }
+        System.out.println("");
+        // =================================================
+        System.out.println("C:");
+        text = "Hi Students!";
+
+        System.out.println("Original Text: " + text);
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (isVowel(c)) {
+                System.out.println("'" + c + "' is at index: " + i);
+            }
+        }
+        //==================================================
+        System.out.println("\nD:");
+        String text1 = "Hi Students!";
+        String text2 = "Students";
+        String text3 = "Hi Students!";
+
+        System.out.println("text1: " + text1);
+        System.out.println("text2: " + text2);
+        System.out.println("text3: " + text3 + "\n");
+        System.out.println("text1 is equal to text2: " + text1.equals(text2));
+        System.out.println("text1 is equal to text3: " + text1.equals(text3));
+        //==================================================
+        System.out.println("\nE:");
+        text1 = "Hi Students!";
+        text2 = "Students";
+        text3 = "Teacher";
+
+        System.out.println("text1: " + text1);
+        System.out.println("text2: " + text2);
+        System.out.println("text3: " + text3 + "\n");
+        System.out.println("text1 contains text2: " + text1.contains(text2));
+        System.out.println("text1 contains text3: " + text1.contains(text3));
     }
 }
---
+++ OOP/Lab1/ex6.java
@@ -0,0 +1,39 @@
+package Lab1;
+
+public class ex6 {
+    
+    class Employee {
+        int id;
+        String name;
+        char gender;
+        int age;
+        int phone;
+    }
+
+    class Car {
+        String model;
+        int year;
+        String color;
+        float price;
+        int mileage;
+    }
+
+    class BankAccount {
+        String email;
+        String password;
+        String username;
+        double balance;
+        boolean isRiel;
+    }
+
+    class Video {
+        String title;
+        String fileName;
+        int duration;
+        float fps;
+        int sizeKB;
+    }
+
+    public static void main(String[] args) {
+    }
+}
