bd152ae8c9b120406f707c08f4bf6ee1c37f448d
Hout-Manut <Huotmanut00@gmail.com>
2023-10-03 23:29:33 +0700

    Random commit

---
+++ OOP/Lab2/IDCard.java
@@ -0,0 +1,14 @@
+class IDCard {
+    int id;
+    String name;
+    String address;
+    String phone;
+    int age;
+    String nationality;
+}
+
+public class IDCardTest {
+    public static void main(String[] args) {
+    }
+
+}
