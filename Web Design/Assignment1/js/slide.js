3936c77a2ed17fd4ae5a7c525048cc2270a4499b
MrrDevils <Huotmanut0@gmail.com>
2023-10-26 10:21:39 +0700

    Web

--- Web Design/Assignment1/js/slide.js
+++ Web Design/Assignment1/js/slide.js
@@ -1,28 +1,33 @@
 let slideIndex = 1;
+let timer;
 showSlides(slideIndex);
 
 // Next/previous controls
 function changeSlides(n) {
-  showSlides(slideIndex += n);
+	clearTimeout(timer);
+	
+	showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+	showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slide");
-  let bars = document.getElementsByClassName("bar");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < bars.length; i++) {
-    bars[i].className = bars[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  bars[slideIndex-1].className += " active";
+	let i;
+	let slides = document.getElementsByClassName("slide");
+	if (n > slides.length) {
+		slideIndex = 1;
+	} else if (n < 1) {
+		slideIndex = slides.length;
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slides[slideIndex - 1].style.display = "block";
+	
+	// timer = setTimeout(function () {
+	// 	changeSlides(1);
+	// }, 5000);
 }
