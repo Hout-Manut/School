d5b35c8da91db648e55ec14078fe394898f9c5dc
Hout-Manut <huotmanut00@gmai.com>
2024-10-17 09:45:23 +0700

    Week 2 Session 2 Practice

--- Flutter/Assignments/S3-PRACTICE-Micro-Project-1/quiz.dart
+++ Flutter/Assignments/S3-PRACTICE-Micro-Project-1/quiz.dart
@@ -1,10 +1,53 @@
-enum QuestionType{ SINGLE, CHOISE }
+enum QuestionType { SINGLE, MULTI }
 
 class Question {
   final String title;
-  final String answer;
+  final QuestionType type;
+  final List<Choice> _answers;
+  final List<Choice> availibleChoices;
 
-  Question({required this.title, required this.answer});
+  Question(
+      {required this.title,
+      required this.type,
+      required List<Choice> answers,
+      required List<Choice> choices})
+      : _answers = answers,
+        availibleChoices = choices;
+
+  Question.single(
+      {required this.title,
+      required Choice answers,
+      required List<Choice> choices})
+      : type = QuestionType.SINGLE,
+        _answers = [answers],
+        availibleChoices = choices;
+  Question.multi(
+      {required this.title,
+      required List<Choice> answers,
+      required List<Choice> choices})
+      : type = QuestionType.MULTI,
+        _answers = answers,
+        availibleChoices = choices;
+
+
+}
+
+
+class Result {
+  final Question question;
+  final List<Choice> choices;
+
+  Result({required this.question, required this.choices});
+
+
+}
+
+
+class Choice {
+  final String name;
+  final dynamic value;
+
+  Choice({required this.name, required this.value});
 }
 
 class Player {
@@ -14,11 +57,22 @@ class Player {
 
   Player({required this.firstName, required this.lastName, required this.quiz});
 
-  void answer({required Question question, required String answer}) {
-
-  }
+  void answer({required Question question, required List<Choice> choices}) {}
 }
 
 class Quiz {
   final List<Player> players = [];
+
+  Player createPlayer({required String firstName, required String lastName}) {
+    return Player(firstName: firstName, lastName: lastName, quiz: this);
+  }
+
+  void answer({required Player player, required Question question, required List<Choice> choices}) {
+
+  }
+}
+
+
+void main() {
+
 }
