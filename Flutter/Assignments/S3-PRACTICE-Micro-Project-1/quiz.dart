6d5cbf2c2444ecb7689cdcbb2292e0fdb273dcad
Hout Manut <huotmanut00@gmail.com>
2024-10-17 14:31:44 +0700

    Update quiz.dart

--- Flutter/Assignments/S3-PRACTICE-Micro-Project-1/quiz.dart
+++ Flutter/Assignments/S3-PRACTICE-Micro-Project-1/quiz.dart
@@ -1,10 +1,11 @@
 enum QuestionType { SINGLE, MULTI }
 
-/// A class made to store answers and provide comparation
+/// A class made to store choices/answers and provide comparision
 class Choices {
-  final List<Choice> _answers;
+  final Set<Choice> _answers;
 
-  Choices(List<Choice> answers) : _answers = answers;
+  Choices(Iterable<Choice> answers) : _answers = answers.toSet();
+  Choices.one(Choice answers) : _answers = {answers};
 
   int get length => _answers.length;
 
@@ -19,6 +20,11 @@ class Choices {
 
     return true;
   }
+
+  @override
+  String toString() {
+    return _answers.toString();
+  }
 }
 
 class Choice {
@@ -31,34 +37,51 @@ class Choice {
   bool operator ==(covariant Choice other) {
     return name == other.name && _value == other._value;
   }
+
+  @override
+  String toString() {
+    return "Choice($name)";
+  }
 }
 
 class Question {
+  final int _id;
   final String title;
-  final QuestionType type;
+  final QuestionType type; // Might be redundant
   final Choices _correctAnswers;
   final Choices availibleChoices;
 
-  Question(
-      {required this.title,
-      required this.type,
-      required Choices answers,
-      required Choices choices})
-      : _correctAnswers = answers,
-        availibleChoices = choices;
+  Question({
+    required int id,
+    required this.title,
+    required this.type,
+    required Choices answers,
+    required Choices choices,
+  })  : _id = id,
+        _correctAnswers = answers,
+        availibleChoices = choices {
+    // if (type == QuestionType.SINGLE && _correctAnswers.length != 1)
+    //   throw "Error: invalid Question arguments. Expected 1 answer.";
+    // else if (type == QuestionType.MULTI && _correctAnswers.length == 1)
+    //   throw "Error: invalid Question arguments. Expected multiple answers.";
+  }
 
   Question.single({
+    required int id,
     required this.title,
     required Choice answers,
     required Choices choices,
-  })  : type = QuestionType.SINGLE,
-        _correctAnswers = Choices([answers]),
+  })  : _id = id,
+        type = QuestionType.SINGLE,
+        _correctAnswers = Choices({answers}),
         availibleChoices = choices;
   Question.multi({
+    required int id,
     required this.title,
     required Choices answers,
     required Choices choices,
-  })  : type = QuestionType.MULTI,
+  })  : _id = id,
+        type = QuestionType.MULTI,
         _correctAnswers = answers,
         availibleChoices = choices;
 
@@ -67,17 +90,30 @@ class Question {
     String ans = _correctAnswers.length == 1 ? "Answer" : "Answers";
     return "Question($title, $type, $ans(REDACTED))";
   }
+
+  Result answer({required Choices answers, required Player player}) {
+    return Result(question: this, choices: answers, player: player);
+  }
 }
 
 class Result {
   final Question question;
   final Choices choices;
+  final Player player;
 
-  Result({required this.question, required this.choices});
+  Result({
+    required this.question,
+    required this.choices,
+    required this.player,
+  });
 
   bool get isCorrect => choices == question._correctAnswers;
-}
 
+  @override
+  String toString() {
+    return "Result(correct: $isCorrect, $question, $choices)";
+  }
+}
 
 class Player {
   final String firstName;
@@ -90,33 +126,77 @@ class Player {
     required this.quiz,
   });
 
-  // Result answer({required Question question, required List<Choice> choices}) {}
+  @override
+  bool operator ==(covariant Player other) {
+    return firstName == other.firstName && lastName == other.lastName;
+  }
+
+  Result answer({
+    required Choices choices,
+    Question? question,
+    int? questionId,
+  }) {
+    return quiz.answer(
+      player: this,
+      choices: choices,
+      question: question,
+      questionId: questionId,
+    );
+  }
 }
 
 class Quiz {
-  final List<Player> players = [];
+  final Set<Player> players = {};
+  final Set<Question> questions = {};
 
   Player createPlayer({required String firstName, required String lastName}) {
-    return Player(firstName: firstName, lastName: lastName, quiz: this);
+    Player newPlayer =
+        Player(firstName: firstName, lastName: lastName, quiz: this);
+    players.forEach((p) {
+      if (p == newPlayer) throw "Name already taken.";
+    });
+    players.add(newPlayer);
+    return newPlayer;
   }
 
-  void answer({
+  void addQuestions(Question question) => questions.add(question);
+
+  Result answer({
     required Player player,
-    required Question question,
-    required List<Choice> choices,
-  }) {}
+    required Choices choices,
+    Question? question,
+    int? questionId,
+  }) {
+    if (question == null && questionId == null)
+      throw "Error: invalid Quiz.answer() arguments. Requires `question` or `questionId` to be entered.";
+    else if (question != null) return question.answer(answers: choices, player: player);
+
+    for (Question q in questions) {
+      if (q._id == questionId) return q.answer(answers: choices, player: player);
+    }
+    throw "Error: no questions found with id $questionId.";
+  }
 }
 
 void main() {
-  Choice c1 = Choice(name: "Choice 1", value: 0);
-  Choice c2 = Choice(name: "Choice 2", value: 2);
-  Choice c3 = Choice(name: "Choice 3", value: "4");
-  Choice c4 = Choice(name: "Choice 4", value: "8");
+  Quiz quiz = Quiz();
 
-  Choices c1s = Choices([c1, c2]);
-  Choices c2s = Choices([c1, c2]);
-  Choices c3s = Choices([c1]);
-  Choices c4s = Choices([c3]);
+  Player p1 = quiz.createPlayer(firstName: "Manut", lastName: "Hout");
 
-  print(c1s == c2s);
+  quiz.addQuestions(
+    Question(
+      id: 0,
+      title: "Whats 9 + 10?",
+      type: QuestionType.SINGLE,
+      answers: Choices.one(Choice(name: "21", value: 21)),
+      choices: Choices({
+        Choice(name: "19", value: 19),
+        Choice(name: "20", value: 20),
+        Choice(name: "21", value: 21),
+      }),
+    ),
+  );
+
+  print(p1.answer(
+      choices: Choices.one(Choice(name: "21", value: 21)), questionId: 0));
 }
