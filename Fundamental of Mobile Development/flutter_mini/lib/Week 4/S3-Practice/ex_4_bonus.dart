5d7f6cde9376dc322cb9b5990e29ee366d789efd
Hout-Manut <huotmanut00@gmai.com>
2024-11-03 13:56:33 +0700

    Gradient is working yey

--- Fundamental of Mobile Development/flutter_mini/lib/Week 4/S3-Practice/ex_4_bonus.dart
+++ Fundamental of Mobile Development/flutter_mini/lib/Week 4/S3-Practice/ex_4_bonus.dart
@@ -9,24 +9,57 @@ void main() {
       ),
       backgroundColor: Colors.lightGreen,
       body: Center(
-        child: SizedBox(
-          width: 700,
-          child: Column(
-            children: [
-              ProgressCard(
-                "Saihong O' Meter",
-                gradient: [
-                  Colors.red,
-                  Colors.orange,
-                  // Colors.yellow,
-                  // Colors.green,
-                  // Colors.blue,
-                  // Colors.indigo,
-                  // Colors.purple,
-                ],
-                maxValue: 20,
-              )
-            ],
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: SizedBox(
+            width: 700,
+            child: Column(
+              children: [
+                ProgressCard(
+                  "My score in Flutter",
+                  gradient: [
+                    Colors.green[200]!,
+                    Colors.green,
+                    Colors.green[900]!,
+                  ],
+                  maxValue: 9,
+                ),
+                SizedBox(height: 10),
+                ProgressCard(
+                  "My score in Dart",
+                  gradient: [
+                    Colors.green[200]!,
+                    Colors.green,
+                    Colors.green[900]!,
+                  ],
+                  maxValue: 9,
+                ),
+                SizedBox(height: 10),
+                ProgressCard(
+                  "My score in React",
+                  gradient: [
+                    Colors.green[200]!,
+                    Colors.green,
+                    Colors.green[900]!,
+                  ],
+                  maxValue: 9,
+                ),
+                SizedBox(height: 10),
+                ProgressCard(
+                  "Saihong O' Meter",
+                  gradient: [
+                    Colors.red,
+                    Colors.orange,
+                    Colors.yellow,
+                    Colors.green,
+                    Colors.blue,
+                    Colors.indigo,
+                    Colors.purple,
+                  ],
+                  maxValue: 50,
+                )
+              ],
+            ),
           ),
         ),
       ),
@@ -42,6 +75,10 @@ class ProgressCard extends StatefulWidget {
   final int? defaultValue;
   final int maxValue;
 
+  /// The minimum value from 0.0 to 1.0 of the range you can set to.
+  /// Setting this to above 0.0 will make the bar never empty.
+  final double minimumAllowedFactor;
+
   const ProgressCard(
     this.title, {
     this.color = Colors.blue,
@@ -49,6 +86,7 @@ class ProgressCard extends StatefulWidget {
     this.minValue = 0,
     this.defaultValue,
     this.maxValue = 9,
+    this.minimumAllowedFactor = 0.01,
     super.key,
   });
 
@@ -59,64 +97,78 @@ class ProgressCard extends StatefulWidget {
 class _ProgressState extends State<ProgressCard> {
   late int currentValue;
   late double extrusionAmount;
-  late int extrudeTime;
+  final int extrudeTime = 90;
+  double minimumAllowedFactor = 0.01;
   static const double basePadding = 20;
+  static const double borderRadius = 16;
 
   double leftPadding = basePadding;
   double rightPadding = basePadding;
-  int get steps => widget.maxValue - widget.minValue;
 
   void calculateExtrusionAmount() {
-    if (steps <= 1) {
-      extrusionAmount = 15.0;
-    } else if (steps >= 100) {
-      extrusionAmount = 2.0;
+    if (steps <= 20) {
+      extrusionAmount = 10.0;
+    } else if (steps >= 80) {
+      extrusionAmount = 5.0;
     } else {
-      extrusionAmount = 15 - (17 * ((steps - 1) / 99));
+      extrusionAmount = 2.0;
     }
 
-    if (extrusionAmount > 10) {
-      extrudeTime = 70;
-    } else {
-      extrudeTime = 90;
-    }
+    // if (extrusionAmount > 10) {
+    //   extrudeTime = 80;
+    // } else {
+    //   extrudeTime = 90;
+    // }
   }
 
   @override
   void initState() {
     currentValue = widget.defaultValue ?? widget.minValue;
+    minimumAllowedFactor = widget.minimumAllowedFactor;
     calculateExtrusionAmount();
     super.initState();
   }
 
   void resetPadding() {
     setState(() {
+      minimumAllowedFactor = widget.minimumAllowedFactor;
       leftPadding = basePadding;
       rightPadding = basePadding;
     });
   }
 
+  int get steps => widget.maxValue - widget.minValue;
+
   /// The normalized value from 0.0 to 1.0.
   double get currentFactor {
-    return currentValue / widget.maxValue;
+    double factor = currentValue / widget.maxValue;
+    if (factor <= minimumAllowedFactor) return minimumAllowedFactor;
+    return factor;
   }
 
   Color get color {
     List<Color>? colors = widget.gradient;
     Color? finalColor;
-    if (colors == null) return widget.color;
-    if (currentFactor == 1.0) return colors.last;
+    if (colors == null) {
+      return widget.color;
+    } else if (colors.length == 1) {
+      return colors[0];
+    } else if (currentFactor == 1.0) {
+      return colors.last;
+    }
 
-    int colorIndex = 1;
+    double stepSize = 1.0 / (colors.length - 1);
 
-    for (int i = 1; i < steps; i++) {
-      if (currentFactor < (colorIndex / (colors.length - 1))) {
-        finalColor = Color.lerp(
-            colors[colorIndex - 1], colors[colorIndex], currentFactor / (colors.length - 1));
-        // finalColor = colors[i];
+    for (int i = 1; i < colors.length; i++) {
+      double startFactor = (i - 1) * stepSize;
+      double endFactor = i * stepSize;
+
+      if (currentFactor >= startFactor && currentFactor <= endFactor) {
+        double localFactor =
+            (currentFactor - startFactor) / (endFactor - startFactor);
+        finalColor = Color.lerp(colors[i - 1], colors[i], localFactor);
         break;
       }
-      colorIndex++;
     }
 
     return finalColor ?? colors.last;
@@ -126,7 +178,8 @@ class _ProgressState extends State<ProgressCard> {
     setState(() {
       if (currentValue == widget.maxValue) {
         rightPadding = basePadding - extrusionAmount;
-        leftPadding = basePadding + extrusionAmount;
+        leftPadding = basePadding + extrusionAmount / 2;
+        minimumAllowedFactor = 0;
         return;
       }
       currentValue += 1;
@@ -137,7 +190,8 @@ class _ProgressState extends State<ProgressCard> {
     setState(() {
       if (currentValue == widget.minValue) {
         leftPadding = basePadding - extrusionAmount;
-        rightPadding = basePadding + extrusionAmount;
+        rightPadding = basePadding + extrusionAmount / 2;
+        minimumAllowedFactor = 0;
         return;
       }
       currentValue -= 1;
@@ -150,7 +204,7 @@ class _ProgressState extends State<ProgressCard> {
       padding: EdgeInsets.symmetric(vertical: 20),
       decoration: BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.circular(16),
+        borderRadius: BorderRadius.circular(24),
       ),
       width: 400,
       child: Column(
@@ -201,7 +255,7 @@ class _ProgressState extends State<ProgressCard> {
             onEnd: resetPadding,
             child: Container(
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
+                borderRadius: BorderRadius.circular(borderRadius),
               ),
               clipBehavior: Clip.antiAlias,
               height: 48,
@@ -210,7 +264,7 @@ class _ProgressState extends State<ProgressCard> {
                   Container(
                     decoration: BoxDecoration(
                       border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(16),
+                      borderRadius: BorderRadius.circular(borderRadius),
                     ),
                   ),
                   Align(
